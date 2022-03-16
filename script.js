4c004f7ca35154fb9d7ed61368f5959483ba97b6
rachael <rachaelstevens92@gmail.com>
2022-03-16 11:45:07 +0000

    testing

---
+++ script.js
@@ -0,0 +1,8 @@
+const redButton = document.getElementById(red-button);
+const blueButton = document.getElementById(blue-button);
+const yellowButton = document.getElementById(yellow-button);
+const greenButton = document.getElementById(green-button);
+
+redButton.addEventListener(focus, () => {
+    redButton.style.backgroundColor = "darkred";
+});
